61222f85ed87884a0cfc3e0bd0bea543038cb8bb
Kinnari-2004 <kinnarivaghela2004@gmail.com>
2025-09-17 23:48:02 +0530

    Final

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kinnarivaghela2004:kinnarivaghela2004@cluster0.qqje8x7.mongodb.net/shree-kitchen').then(()=>console.log("DB connected"));
-}
--- backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { loginUser,registeredUser } from "../controllers/userController.js"
-
-const userRouter = express.Router()
-
-userRouter.post("/register",registeredUser)
-userRouter.post("/login", loginUser)
-
-export default userRouter;
